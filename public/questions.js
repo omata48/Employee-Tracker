1341d1829bf43ad993cfa8d4f70aad0f11151100
Omar <62365988+omata48@users.noreply.github.com>
2020-09-02 22:45:30 -0500

    exporting functions and questions

---
+++ public/questions.js
@@ -0,0 +1,74 @@
+module.exports = {
+  addToQuestions: [
+    {
+      name: "route",
+      type: "list",
+      message: "Add to:",
+      choices: ["Departments", "Roles", "Employees"],
+    },
+    {
+      name: "name",
+      type: "input",
+      message: "Department Name",
+      when: function (answer) {
+        return answer.route === "Departments";
+      },
+    },
+    {
+      name: "title",
+      type: "input",
+      message: "Role title",
+      when: function (answer) {
+        return answer.route === "Roles";
+      },
+    },
+    {
+      name: "salary",
+      type: "number",
+      message: "Salary of role",
+      when: function (answer) {
+        return answer.route === "Roles";
+      },
+    },
+    {
+      name: "department_id",
+      type: "number",
+      message: "Department ID of role",
+      when: function (answer) {
+        return answer.route === "Roles";
+      },
+    },
+    {
+      name: "first_name",
+      type: "input",
+      message: "Employee first name",
+      when: function (answer) {
+        return answer.route === "Employees";
+      },
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "Employee last name",
+      when: function (answer) {
+        return answer.route === "Employees";
+      },
+    },
+    {
+      name: "role_id",
+      type: "number",
+      message: "Employee's role ID",
+      when: function (answer) {
+        return answer.route === "Employees";
+      },
+    },
+    {
+      name: "manager_id",
+      type: "number",
+      message: "Employee's manager_id",
+      when: function (answer) {
+        return answer.route === "Employees";
+      },
+    },
+  ]
+}
